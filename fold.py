6e00817ca70376de62a58f11e6379bd53420d237
Joe Drago <jdrago@netflix.com>
2015-03-20 10:10:11 -0700

    inverted fold

--- fold.py
+++ fold.py
@@ -36,3 +36,36 @@ class FoldToLinesCommand(FilterToLinesCommand):
 
     def prepare_output_line(self, line, matches):
         pass
+
+
+class PromptFoldToLinesInvertedCommand(PromptFilterToLinesCommand):
+
+    def run(self, search_type = 'string'):
+        self._run(search_type, "fold_to_lines_inverted", "Fold")
+
+
+class FoldToLinesInvertedCommand(FilterToLinesCommand):
+
+    def show_filtered_lines(self, edit, lines):
+        source_lines = self.view.lines(sublime.Region(0, self.view.size()))
+        filtered_line_numbers = [self.view.rowcol(line.begin())[0] for line, _ in lines]
+        regions = []
+        region = None
+        for line in source_lines:
+            matched = (self.view.rowcol(line.begin())[0] in filtered_line_numbers) ^ self.invert_search
+            if not matched:
+                if region:
+                    regions.append(region)
+                    region = None
+            else:
+                if region:
+                    region = region.cover(line)
+                else:
+                    region = sublime.Region(line.begin(), line.end())
+        if region:
+            regions.append(region)
+        if regions:
+            self.view.fold(regions)
+
+    def prepare_output_line(self, line, matches):
+        pass
